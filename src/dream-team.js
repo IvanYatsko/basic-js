c30ae61588082a9a2e28b4609f9c6a4948f90bf7
Yatsko Ivan <ivanyatsko@mail.ru>
2021-03-28 01:31:04 +0300

    feat: realization function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  members = members.filter(n => typeof n === 'string');
+  members = members.map((x) => {
+    return x.toUpperCase().trim().slice(0, 1);
+  });
+  members = members.sort().join('');
+  return members;
 };
